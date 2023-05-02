f6dc779b43dcd91d62b142691a060eea50f07c76
nimdy <nimdavirus@gmail.com>
2023-05-01 20:49:20 -0600

    🐛 fix(menu.py): download missing files instead of skipping them ✨ feat(menu.py): add support for Nimdy's login form and authentication utils

--- menu.py
+++ menu.py
@@ -836,7 +836,8 @@ def add_nimdys_login_form():
     # Download and add LoginForm.tsx to chatbot-ui/Settings/
     login_form_path = os.path.join(chatbot_ui_path, "components/Settings/LoginForm.tsx")
     if not os.path.exists(login_form_path):
-        print("LoginForm.tsx not found. Skipping.")
+        download_file("https://github.com/Nimdy/chatgpt-menu-installer/raw/main/plugins/LoginForm.tsx",
+                    login_form_path)
     else:
         shutil.copy(login_form_path, login_form_path + ".bak")
         download_file("https://github.com/Nimdy/chatgpt-menu-installer/raw/main/plugins/LoginForm.tsx",
@@ -845,7 +846,8 @@ def add_nimdys_login_form():
     # Download and replace _app.tsx in chatbot-ui/pages/
     app_path = os.path.join(chatbot_ui_path, "pages/_app.tsx")
     if not os.path.exists(app_path):
-        print("_app.tsx not found. Skipping.")
+        download_file("https://github.com/Nimdy/chatgpt-menu-installer/raw/main/plugins/_app.tsx",
+                    app_path)
     else:
         shutil.copy(app_path, app_path + ".bak")
         download_file("https://github.com/Nimdy/chatgpt-menu-installer/raw/main/plugins/_app.tsx",
@@ -854,10 +856,13 @@ def add_nimdys_login_form():
     # Download and add auth.ts in chatbot-ui/utils/app
     auth_path = os.path.join(chatbot_ui_path, "utils/app/auth.ts")
     if not os.path.exists(auth_path):
-        print("auth.ts not found. Skipping.")
-    else:
         download_file("https://github.com/Nimdy/chatgpt-menu-installer/raw/main/plugins/auth.ts",
                     auth_path)
+    else:
+        shutil.copy(auth_path, auth_path + ".bak")
+        download_file("https://github.com/Nimdy/chatgpt-menu-installer/raw/main/plugins/auth.ts",
+                    auth_path)
+
 
     # Take input for each var or accept defaults
     env_vars = {
